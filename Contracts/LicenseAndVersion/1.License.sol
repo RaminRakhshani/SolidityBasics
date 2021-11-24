e5e3d02a12593a2db19a815238185d4eb4d22380
Ramin Rakhshani <RaminRakhshani@outlook.com>
2021-11-24 22:26:18 +0330

    version

--- Contracts/LicenseAndVersion/1.License.sol
+++ Contracts/LicenseAndVersion/1.License.sol
@@ -1,3 +1,12 @@
 //SPDX-License-Identifier: GPL-3.0
 
+//1.This is a pragma line
 
+// The version of solidity is 0.8.10. The compiler exactly use the version of 0.8.10.
+pragma solidity 0.8.10;
+
+// the version of solidity is 0.8.10 or higher. That will explain to it that we can now compile this code with 0.8.10 and higher like 0.9.0, ...
+pragma solidity ^0.8.10;
+
+//the solidity version is between two version.
+pragma solidity >=0.8.0 <0.9.0;
