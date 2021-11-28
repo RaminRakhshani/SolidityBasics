59174d2d917dbd370ec7fb5fa1810aebfeac6da9
Ramin Rakhshani <RaminRakhshani@outlook.com>
2021-11-28 23:43:49 +0330

    Inheritance

---
+++ Contracts/8.Inheritance.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+/// @title NatSpec
+/// @author Ramin Rakhshani
+/// @notice This is sample of NatSpec comment
+/// @dev  Some Descriptions
+/// @custom:experimental This is an experimental contract
+contract Variables {
+    uint256 public number = 20; //state variable
+    bool private paused = true; //state variable
+    uint256 internal time = block.timestamp; // state variable saving a global variable
+
+    function myFunc() public view {
+        uint256 localVariable = 20; //local variable
+        bool localPaused = true; //local variable
+        address theSenderAddress = msg.sender; //local variable saving a global variable
+    }
+}
+
+contract INeedVariables is Variables {
+    uint256 public theAwesomeVar = 100;
+    uint256 public theTime = time;
+}
